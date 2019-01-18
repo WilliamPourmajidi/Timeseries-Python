6886c78065afbebd129c4e1b83d3a3cc3f809d9b
William Pourmajidi <William.Pourmajidi@gmail.com>
2019-01-18 11:26:27 -0500

    Second Commit

--- ProjectMain.py
+++ ProjectMain.py
@@ -45,84 +45,65 @@ from fbprophet import Prophet
 
 
 ####
-
-def is_stationary(df, maxlag=14, autolag=None, regression='ct'):
-    """Run the Augmented Dickey-Fuller test from statsmodels and print output.
-    """
-    outpt = stt.adfuller(df,maxlag=maxlag, autolag=autolag,
-                            regression=regression)
-    print('adf\t\t {0:.3f}'.format(outpt[0]))
-    print('p\t\t {0:.3g}'.format(outpt[1]))
-    print('crit. val.\t 1%: {0:.3f}, \
-5%: {1:.3f}, 10%: {2:.3f}'.format(outpt[4]["1%"],
-                                     outpt[4]["5%"], outpt[4]["10%"]))
-    print('stationary?\t {0}'.format(['true', 'false']\
-                                   [outpt[0]>outpt[4]['5%']]))
-    return outpt
+#
+# def is_stationary(df, maxlag=14, autolag=None, regression='ct'):
+#     """Run the Augmented Dickey-Fuller test from statsmodels and print output.
+#     """
+#     outpt = stt.adfuller(df,maxlag=maxlag, autolag=autolag,
+#                             regression=regression)
+#     print('adf\t\t {0:.3f}'.format(outpt[0]))
+#     print('p\t\t {0:.3g}'.format(outpt[1]))
+#     print('crit. val.\t 1%: {0:.3f}, \
+# 5%: {1:.3f}, 10%: {2:.3f}'.format(outpt[4]["1%"],
+#                                      outpt[4]["5%"], outpt[4]["10%"]))
+#     print('stationary?\t {0}'.format(['true', 'false']\
+#                                    [outpt[0]>outpt[4]['5%']]))
+#     return outpt
 
 
 
 ############### Import Data Set #############
-
-# dateparse = lambda d: pd.datetime.strptime(d, '%Y-%m-%d')
-# temp = pd.read_csv('mean-daily-temperature-fisher-river2.csv',
-#                    parse_dates=['Date'],
-#                    index_col='Date',
-#                    date_parser=dateparse,
-#                    )
-
-
-df = pd.read_csv('stationary.csv',
-                 parse_dates=['ds'],
-                 index_col='ds',
-                 delimiter=','
+df = pd.read_csv('grafana_data_export-2017-10-09-to-2017-10-23.csv',
+                 parse_dates=['Time'],
+                 index_col='Value',
+                 delimiter=';'
                  )
-
 
 print("\n#### Information about the imported Time Series and its fields####\n" )
 df.info()
 
-print("\n##### Top 5 rows ####\n", df.head())
-
-df = df.iloc[:, 0]  # Purely integer-location based indexing for selection by position.
+# print("\n##### Top 5 rows ####\n", df.head())
+#
+# df = df.iloc[:, 0]  # Purely integer-location based indexing for selection by position.
 
 print("\n##### Indexing on Time! ####\n")
-
 print(df.index)
-
 print("\n#####Statsicical details of the time series ####\n", df.describe())
 
-print("\n#####Checking the stationrity of the time series based on Augmented Dickey-Fuller ####\n" )
-print(is_stationary(df))
+#print("\n#####Checking the stationrity of the time series based on Augmented Dickey-Fuller ####\n" )
+#print(is_stationary(df))
 
-moving_avg = pd.rolling_mean(df,12)
-decomposition = seasonal_decompose(df,freq=50)
-trend = decomposition.trend
-seasonal = decomposition.seasonal
-residual = decomposition.resid
+# moving_avg = pd.rolling_mean(df,12)
+# decomposition = seasonal_decompose(df,freq=50)
+# trend = decomposition.trend
+# seasonal = decomposition.seasonal
+# residual = decomposition.resid
 
-plt.plot(df)
-plt.plot(moving_avg, label="Moving Average", color='red')
-plt.plot(trend, label='Trend', color='yellow')
+#plt.plot(df)
 # plt.plot(seasonal,label='Seasonality',color='orange')
 
-plt.legend(['Raw', 'Moving Average','Trend'])
+#plt.legend(['Raw', 'Moving Average','Trend'])
 
-plt.show()
-
+#plt.show()
 
 ### Prophet Forcasting
-df2 = pd.read_csv('Stationary.csv', delimiter=',')
-df2['y'] = np.log(df2['y'])
-
-
+df[] = np.log(df[])
 
 my_model = Prophet(interval_width=0.95)
-
-my_model.fit(df2)
+my_model.fit(df)
 
 #future_dates = my_model.make_future_dataframe(periods=3, freq='MS')
-future_dates = my_model.make_future_dataframe(periods=30)
+future_dates = my_model.make_future_dataframe(periods=10)
 
 future_dates.tail()
 
@@ -138,9 +119,7 @@ plt.show()
 
 
 
-
-
-plotly.tools.set_credentials_file(username='salman.pourmajidi', api_key='1LdgMkdGWb4TodAi8Y9S')
+plotly.tools.set_credentials_file(username='william.pourmajidi', api_key='a5YwCgFUNAi4DRxLh2Vc')
 sns.set(style="white", color_codes=True)
 
 pr = go.Scatter(
