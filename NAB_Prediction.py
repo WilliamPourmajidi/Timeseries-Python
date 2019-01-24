ab761089056f0c2b9bc3f1d516444e59d3768791
William Pourmajidi <William.Pourmajidi@gmail.com>
2019-01-24 15:08:44 -0500

    Second Commit

--- NAB_Prediction.py
+++ NAB_Prediction.py
@@ -11,18 +11,21 @@ import numpy as np
 from fbprophet import Prophet
 import csv
 import os
-plt.style.use(os.path.join(os.getcwd(), 'mystyle.mplstyle') )
+
+plt.style.use(os.path.join(os.getcwd(), 'mystyle.mplstyle'))
 plt.rcParams['axes.edgecolor'] = 'w'
 import warnings  # current version of seaborn generates a bunch of warnings that we'll ignore
+
 warnings.filterwarnings("ignore")
 import seaborn as sns
 import plotly.plotly as py
 import plotly.graph_objs as go
-#from pandas.io import data, wb
+# from pandas.io import data, wb
 from pandas_datareader import data, wb
 from matplotlib.pylab import rcParams
+
 rcParams['figure.figsize'] = 15, 6
-#plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 
 
 # Settings for Panda dataframe displays
@@ -31,21 +34,17 @@ pd.options.display.width = 1200
 pd.options.display.max_colwidth = 100
 pd.options.display.max_columns = 100
 
-
-
 # Import Data Set
 df = pd.read_csv('numenta_ec2_cpu_utilization_5f5533.csv'
-                 #,
-                 #parse_dates=['timestamp'],
-                 #index_col='timestamp',
-                 #delimiter=','
+                 # ,
+                 # parse_dates=['timestamp'],
+                 # index_col='timestamp',
+                 # delimiter=','
                  )
 
-trimmed_df = df[['timestamp', 'value']] # Trimming columns to timestamp and value
+trimmed_df = df[['timestamp', 'value']]  # Trimming columns to timestamp and value
 trimmed_df.columns = ['ds', 'y']  # renaming coloumn as Prophet is picky on names!
 print(trimmed_df)
-
-
 
 model = Prophet()  # Creating the model
 model.fit(trimmed_df)  # Fitting the data frame to the model
@@ -53,42 +52,83 @@ model.fit(trimmed_df)  # Fitting the data frame to the model
 # future = model.make_future_dataframe(periods=3)
 # print(future.tail())
 
-
-future = model.make_future_dataframe(periods=10, freq='H')
-
-
-
+future = model.make_future_dataframe(periods=10, freq='5Min')
+print(future)
 forecast = model.predict(future)
 
-
-print("##############Before\n",trimmed_df)
+# print("##############Before\n",trimmed_df)
 trimmed_df['yhat'] = forecast['yhat']
-print("##############After\n",trimmed_df)
-
-# print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
-
-
-fig1 = model.plot(forecast)
-# fig1.show()
-
-
-
-#
-# print("\n#### Information about the imported Time Series and its fields####\n" )
-# df.info()
-#
-# print("\n##### Top 5 rows ####\n", df.head())
-# print(df)
-
-#
+trimmed_df['yhat_lower'] = forecast['yhat_lower']
+trimmed_df['yhat_upper'] = forecast['yhat_upper']
 # #
-# # df = df.iloc[:, 0]  # Purely integer-location based indexing for selection by position.
+# #
+
+print("##############After\n", trimmed_df)
+print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
+
+stdv = np.std(trimmed_df['y'])
+print(stdv)
+upper_anomalyborder = trimmed_df['yhat_upper'] + int(1.5 * stdv)
+lower_anomalyborder = trimmed_df['yhat_lower'] - int(1.5 * stdv)
+trimmed_df['upper_anomalyborder'] = upper_anomalyborder
+trimmed_df['lower_anomalyborder'] = lower_anomalyborder
+print(trimmed_df)
 #
-# print("\n##### Indexing on Time! ####\n")
-# print(df.index)
-# print("\n#####Statsicical details of the time series ####\n", df.describe())
 #
-# #print("\n#####Checking the stationrity of the time series based on Augmented Dickey-Fuller ####\n" )
-# #print(is_stationary(df))
+# # fig1 = model.plot(forecast)
+# # fig1.show()
 #
 #
+plt.figure(figsize=(18, 5))
+plt.title("Prophet(ARIMA)-based Data Analysis", fontsize=20)
+plt.plot(trimmed_df['y'], label='Actual Data')
+plt.plot(trimmed_df['yhat'], label='Predicted Data')
+plt.plot(trimmed_df['yhat_upper'], "r-.", label="Predicted Upper Bound")
+plt.plot(trimmed_df['yhat_lower'], "r-.", label="Predicted Lower Bound")
+plt.plot(trimmed_df['upper_anomalyborder'], label="Upper Anomaly Border")
+plt.plot(trimmed_df['lower_anomalyborder'], label="Lower Anomaly Border")
+plt.legend(loc='best')
+plt.grid(True)
+plt.show()
+
+
+anomalies = pd.DataFrame(index=trimmed_df.index)
+anomalies['Anomalies'] = 0
+
+print(anomalies)
+
+# # updating the anomalies data frame with anomaly values
+for i, row in trimmed_df.iterrows():  # i: dataframe index; row: each row in series format
+    if ((row['y']) > (row['upper_anomalyborder'])):
+        # print("Warning: Anomlay Detected")
+        # anomalies.loc[i]['av'] = row['av']
+
+
+# #
+
+
+# create an empty dataframe with the same index and columns of testing dataset
+# ## Enable for extra plot
+# plt.figure(figsize=(18, 5))
+# plt.title("Simple Average Forecast",fontsize=20)
+# plt.plot(training_set.index, training_set['av'], label='Training Dataset')
+# plt.plot(testing_set.index, testing_set['av'], label='Testing Dataset')
+# plt.plot(y_hat_avg['avg_forecast'], label='Simple Average Forecast')
+#
+# plt.legend(loc='best')
+# plt.grid(True)
+# plt.show()
+
+# updating the anomalies data frame with anomaly values
+# for i, row in y_hat_avg.iterrows():  # i: dataframe index; row: each row in series format
+#     # print(row['av'],i)
+#     if ((row['av']) > (row['upper_bound'])):
+#         anomalies_average.loc[i]['av'] = row['av']
+#
+# plt.figure(figsize=(18, 5))
+
+# plt.plot(anomalies_naive, "ro", markersize=2, label="Anomalies")
+
+
+# plt.plot(testing_set.index, testing_set['av'], label='Testing Dataset')
+# plt.plot(y_hat_naive.index, y_hat_naive['naive'], label='Naive Forecast')
