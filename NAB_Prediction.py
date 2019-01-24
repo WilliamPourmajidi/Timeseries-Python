57aa56f876c99b6cf982e8a972e7e78f500a055b
William Pourmajidi <William.Pourmajidi@gmail.com>
2019-01-24 09:25:23 -0500

    Second Commit

--- NAB_Prediction.py
+++ NAB_Prediction.py
@@ -1,4 +1,3 @@
-
 import scipy.stats as st
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa import stattools as stt
@@ -25,25 +24,74 @@ from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 15, 6
 #plt.style.use('fivethirtyeight')
 
-############### Import Data Set #############
-df = pd.read_csv('grafana_data_export-2017-10-09-to-2017-10-23.csv',
-                 parse_dates=['Time'],
-                 index_col='Value',
-                 delimiter=';'
+
+# Settings for Panda dataframe displays
+
+pd.options.display.width = 1200
+pd.options.display.max_colwidth = 100
+pd.options.display.max_columns = 100
+
+
+
+# Import Data Set
+df = pd.read_csv('numenta_ec2_cpu_utilization_5f5533.csv'
+                 #,
+                 #parse_dates=['timestamp'],
+                 #index_col='timestamp',
+                 #delimiter=','
                  )
 
-print("\n#### Information about the imported Time Series and its fields####\n" )
-df.info()
+trimmed_df = df[['timestamp', 'value']] # Trimming columns to timestamp and value
+trimmed_df.columns = ['ds', 'y']  # renaming coloumn as Prophet is picky on names!
+print(trimmed_df)
 
-# print("\n##### Top 5 rows ####\n", df.head())
+
+
+model = Prophet()  # Creating the model
+model.fit(trimmed_df)  # Fitting the data frame to the model
+
+# future = model.make_future_dataframe(periods=3)
+# print(future.tail())
+
+
+future = model.make_future_dataframe(periods=10, freq='H')
+
+
+
+
+forecast = model.predict(future)
+
+
+print("##############Before\n",trimmed_df)
+trimmed_df['yhat'] = forecast['yhat']
+print("##############After\n",trimmed_df)
+
+
+
+# print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']])
+
+
+fig1 = model.plot(forecast)
+# fig1.show()
+
+
+
 #
-# df = df.iloc[:, 0]  # Purely integer-location based indexing for selection by position.
+# print("\n#### Information about the imported Time Series and its fields####\n" )
+# df.info()
+#
+# print("\n##### Top 5 rows ####\n", df.head())
+# print(df)
 
-print("\n##### Indexing on Time! ####\n")
-print(df.index)
-print("\n#####Statsicical details of the time series ####\n", df.describe())
-
-#print("\n#####Checking the stationrity of the time series based on Augmented Dickey-Fuller ####\n" )
-#print(is_stationary(df))
-
-
+#
+# #
+# # df = df.iloc[:, 0]  # Purely integer-location based indexing for selection by position.
+#
+# print("\n##### Indexing on Time! ####\n")
+# print(df.index)
+# print("\n#####Statsicical details of the time series ####\n", df.describe())
+#
+# #print("\n#####Checking the stationrity of the time series based on Augmented Dickey-Fuller ####\n" )
+# #print(is_stationary(df))
+#
+#
